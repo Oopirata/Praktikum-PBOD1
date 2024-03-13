b5f0f3482e1e70cac4d26b7e325044f0002a360e
Oopirata <126538833+Oopirata@users.noreply.github.com>
2024-03-13 19:32:26 +0700

    Add files via upload

---
+++ Minggu4/direktori_anda/org/poligon/Poligon.java
@@ -0,0 +1,19 @@
+/*
+Nama	: Hanif Herofa
+NIM		: 24060122120015
+File	: Poligon.java
+ */
+ 
+ package org.poligon;
+ 
+ public class Poligon{
+	 protected int jumlahSisi;
+	 
+	 public void setJumlahSisi(int sisi){
+		 this.jumlahSisi = sisi;
+	 }
+	 
+	 public int getJumlahSisi(){
+		 return this.jumlahSisi;
+	 }
+ }
